d28ab74dfa9e7947b61fce7b94a2fe0da3065962
Poleac Liviu Stefan <stefan.liviu28^2yahoo.ro>
2024-01-12 11:32:10 +0200

    toTesting

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 // Configure CORS
 const corsOptions = {
     credentials: true,
-    origin: "https://thler.com", // Update as per your client URL
+    origin: "http://localhost:3000", // Update as per your client URL
     exposedHeaders: ["Set-Cookie"]
 };
 
