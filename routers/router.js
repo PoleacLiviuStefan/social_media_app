1c61c7bac36941002215805b8bd55503d38432e4
Poleac Liviu Stefan <stefan.liviu28^2yahoo.ro>
2024-01-14 23:40:42 +0200

    final

--- routers/router.js
+++ routers/router.js
@@ -4,14 +4,17 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 const passport =require("passport");
+const { v4: uuidv4 } = require("uuid");
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "public/uploads");
   },
   filename: (req, file, cb) => {
+    const uniqueSuffix = uuidv4(); // Unique identifier
     cb(
       null,
-      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+      file.fieldname + "_" + uniqueSuffix + path.extname(file.originalname)
     );
   },
 });
