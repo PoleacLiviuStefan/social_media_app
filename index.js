4d7400253600f46bb0f1e1e25f9b55f293c84fbb
Poleac Liviu Stefan <stefan.liviu28^2yahoo.ro>
2024-01-17 02:43:31 +0200

    modifyForDynamic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require("cors");
@@ -37,6 +38,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/api', router);
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
