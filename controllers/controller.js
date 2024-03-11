3619e6f431da176f55f9819652f6c7ed5773cba5
Poleac Liviu Stefan <stefan.liviu286@gmail.com>
2024-03-11 23:36:58 +0200

    UpdatedDomains

--- controllers/controller.js
+++ controllers/controller.js
@@ -19,8 +19,8 @@ dotenv.config();
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = process.env.JWT_SECRET || "defaultSecret";
 
-const serverURL = "https://www.api.waygital.ro"; //http://localhost:3001
-const clientURL = "https://www.waygital.ro"  //http://localhost:4789
+const serverURL = "https://api.waygital.ro"; //http://localhost:3001
+const clientURL = "https://social.waygital.ro"  //http://localhost:4789
 
 const test = (req, res) => {
   res.send("Test endpoint working");
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config();
 // Configure CORS
 const corsOptions = {
     credentials: true,
-    origin: "https://www.waygital.ro", // Update as per your client URL http://localhost:4789
+    origin: "https://social.waygital.ro", // Update as per your client URL http://localhost:4789
 };
 
 app.use(cors(corsOptions));
